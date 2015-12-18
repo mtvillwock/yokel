10d7e74d3773accea656409acd3de09b89387dce
Matthew Villwock <mtvillwock@gmail.com>
2015-12-18 15:31:28 -0800

    swap parse keys

--- public/interceptors.js
+++ public/interceptors.js
@@ -9,9 +9,9 @@ var app = angular.module('myApp.interceptors', []);
 app.factory('parseInterceptor', function ($rootScope, $q, $window) {
   return {
     request: function (config) {
-      config.headers['X-Parse-Application-Id'] = 'JrC7tEcVEZpJwPgGdEBmRokLG1vv1MAnfdJhkx9V';
-      config.headers['X-Parse-REST-API-Key'] = 'NfRdeQRSOdVJAkLIM4BjAQ1ekZcpuZYbH0GF4SgQ';
-      
+      config.headers['X-Parse-Application-Id'] = 'HRtnGU3AUfDeA9GhQfPzY6qDvG7wEjILO4TTK2AH';
+      config.headers['X-Parse-REST-API-Key'] = 'FVuQU8wncnzB1KySdoL9uzZUtXSZu1D4rzNtpnpL';
+
       // ADD SESSION TOKEN TO REQUESTS
       if (localStorage.getItem('sessionToken')) {
         config.headers['X-Parse-Session-Token'] = localStorage.getItem('sessionToken')
